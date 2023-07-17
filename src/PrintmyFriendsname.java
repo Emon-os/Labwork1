6f8e03ceb8e1911f7f3a1dc07a73beeee08d4e93
Emon Khan <ek369542@gmail.com>
2023-07-17 16:30:56 +0600

    adding files

---
+++ src/PrintmyFriendsname.java
@@ -0,0 +1,9 @@
+public class PrintmyFriendsname {
+    public static void main(String[] args) {
+        Myfriendsname myfriendsname = new Myfriendsname();
+        myfriendsname.tusar();
+        myfriendsname.shoref();
+        myfriendsname.rony();
+    }
+}
+
